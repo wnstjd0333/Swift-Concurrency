6a4fa23860c3efc455852d7ab31487c34221d0bb
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-11-05 21:27:03 +0900

    add: repository

---
+++ Swift-Concurrency/Swift_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ConcurrencyApp.swift
+//  Swift-Concurrency
+//
+//  Created by 金峻聖 on 2022/11/05.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
